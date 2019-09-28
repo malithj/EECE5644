17018b595b7f9d86e95d881ae407d5073921a9f5
Malith <malith.12@cse.mrt.ac.lk>
2019-09-28 15:06:42 -0400

    Added class posterior probability

--- HW1_Code/q4.py
+++ HW1_Code/q4.py
@@ -37,6 +37,28 @@ def main():
     plt.savefig('results/q4.png')
     plt.show()
 
+    s = 1
+    scaling_factor = 0.5 / s
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(111)
+    y1, x1 = np.histogram(dist1, bins=bins, density=True)
+    y2, x2 = np.histogram(dist2, bins=bins, density=True)
+    ax.bar((x1[1:] + x1[:-1]) * .5, y1 * scaling_factor,
+           width=(x1[1] - x1[0]), color="red", alpha=0.7, label=r'p(L=1|x) ~ $N(0, 1)$')
+    ax.bar((x2[1:] + x2[:-1]) * .5, y2 * scaling_factor,
+           width=(x2[1] - x2[0]), color="blue", alpha=0.6, label=r'p(L=2|x) ~ $N(\mu, \sigma^{2})$', )
+
+    ax.legend()
+    ax.set_title("Class Posterior Probability")
+    ax.set_ylabel("Density")
+    ax.set_xlabel("x")
+    ax.axvline(x=result[0], color='k', linestyle='--')
+    ax.text(result[0] - 1.0, 0.20, r'$lower={0:}$'.format(np.round(result[0], 2)))
+    ax.axvline(x=result[1], color='k', linestyle='--')
+    ax.text(result[1] - 1.0, 0.20, r'$upper={0:}$'.format(np.round(result[1], 2)))
+    plt.savefig('results/q4_posterior.png')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
