174b068c034d3e1b23aea5e0ad1fed926f88e7e6
Malith <malith.12@cse.mrt.ac.lk>
2019-10-18 08:40:29 -0400

    Modified gamma range

--- Midterm_Code/q3.m
+++ Midterm_Code/q3.m
@@ -3,14 +3,14 @@ clear all; close all; clc;
 low = -1;
 high = 1;
 N = 10;
-v_sigma = 0.4;       % choose sigma in a way which will increase noise (standard distribution sigma: 1)
+v_sigma = 0.1;       % choose sigma in a way which will increase noise (standard distribution sigma: 1)
 
 % choose real roots to be -1, 0, 1. i.e. x^3 - x is the expression.
 W_true = [1, 0, -1, 0];
 
 % when gamma is large, the prior probability component in MAP approaches zero
 % compute MAP estimates based on gamma
-start = -5; stop = 5;
+start = -2; stop = 2;
 totalRuns = 100;
 squared_error = zeros(stop - start + 1, totalRuns);
 for gamma = logspace(start, stop, stop - start + 1)
@@ -21,19 +21,19 @@ for gamma = logspace(start, stop, stop - start + 1)
         % generate v array
         v_mean = 0;
         v = v_mean + v_sigma * randn(1, N);
-
+        
         % generate the y dataset
         y = x.^3 - x + v;
-
+        
         % define the equations to solve param vector
         syms a b c d;
-        eqd = a * (sum(x.^3)) + b * (sum(x.^2)) + c * (sum(x)) + d * ((1 / (gamma^2)) + (1 / v_sigma^2) * N) == sum(y);
-        eqc = a * (sum(x.^4)) + b * (sum(x.^3)) + d * (sum(x)) + c * ((1 / (gamma^2)) + (1 / v_sigma^2) * (sum(x.^2))) == sum(x.*y);
-        eqb = a * (sum(x.^5)) + c * (sum(x.^3)) + d * (sum(x.^2)) + b * ((1 / (gamma^2)) + (1 / v_sigma^2) * (sum(x.^4))) == sum((x.^2).*y);
-        eqa = b * (sum(x.^5)) + c * (sum(x.^4)) + d * (sum(x.^3)) + a * ((1 / (gamma^2)) + (1 / v_sigma^2) * (sum(x.^6))) == sum((x.^3).*y);
+        eqd = a * (sum(x.^3)) + b * (sum(x.^2)) + c * (sum(x)) + d * ((v_sigma^2 / (gamma^2)) + N) == sum(y);
+        eqc = a * (sum(x.^4)) + b * (sum(x.^3)) + d * (sum(x)) + c * ((v_sigma^2 / (gamma^2)) + (sum(x.^2))) == sum(x.*y);
+        eqb = a * (sum(x.^5)) + c * (sum(x.^3)) + d * (sum(x.^2)) + b * ((v_sigma^2 / (gamma^2)) + (sum(x.^4))) == sum((x.^2).*y);
+        eqa = b * (sum(x.^5)) + c * (sum(x.^4)) + d * (sum(x.^3)) + a * ((v_sigma^2 / (gamma^2)) + (sum(x.^6))) == sum((x.^3).*y);
         sol = solve([eqa, eqb, eqc, eqd], [a, b, c, d]);
         solution = [vpa(sol.a), vpa(sol.b), vpa(sol.c), vpa(sol.d)];
-
+        
         % L2 norm between two vectors
         norm_ = norm(solution - W_true)^2;
         iteration = stop + log10(gamma) + 1;
